88091dd66d17656b451656cfe847d038808d5b10
Saifulin Nail <saifulin-nail@mail.ru>
2023-01-04 22:31:28 +0600

    database2_two

--- 2.2-databases-2/orm_migrations/school/views.py
+++ 2.2-databases-2/orm_migrations/school/views.py
@@ -1,16 +1,12 @@
+from django.db.models import Prefetch
 from django.views.generic import ListView
 from django.shortcuts import render
-
 from .models import Student, Teacher
 
 
 def students_list(request):
     template = 'school/students_list.html'
-    object_list = Student.objects.all()
+    object_list = Student.objects.order_by('group').all()\
+        .prefetch_related(Prefetch('teachers', to_attr='prefetched_student'))
     context = {'object_list': object_list}
-
-    # используйте этот параметр для упорядочивания результатов
-    # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
-    ordering = 'group'
-
     return render(request, template, context)
