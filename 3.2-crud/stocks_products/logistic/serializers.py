d09f3f07b8807228bdf6d3f884638f1ce0b4682f
Saifulin Nail <saifulin-nail@mail.ru>
2023-01-29 19:25:13 +0600

    serializer correct

--- 3.2-crud/stocks_products/logistic/serializers.py
+++ 3.2-crud/stocks_products/logistic/serializers.py
@@ -46,5 +46,5 @@ class StockSerializer(serializers.ModelSerializer):
         stock = super().update(instance, validated_data)
 
         for position in positions:
-            StockProduct.objects.update_or_create(product=position['product'], defaults=position)
+            StockProduct.objects.update_or_create(stock=stock, product=position['product'], defaults=position)
         return stock
